5199d9bcb5a2a28b9464bbbecd36dfa5f170220d
Gatt101 <gauravpatilk11@gmail.com>
2024-12-20 18:30:35 +0530

    nav and sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodoCreateComponent } from './component/todo/todo-create/todo-create.component';
 import { TodoListComponent } from './component/todo/todo-list/todo-list.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,TodoCreateComponent,TodoListComponent],
+  imports: [RouterOutlet,TodoCreateComponent,TodoListComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
